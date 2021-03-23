8a8a237120db2a2ab106563fa29045829a1168b2
Li jq <17600260156@163.com>
2021-03-23 22:42:49 +0800

    循环内容的练习

--- src/main/test/MavenStudentTest.java
+++ src/main/test/MavenStudentTest.java
@@ -11,9 +11,10 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class MavenStudentTest {
-
+    //创建时间
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     Date date = new Date();
     MavenStudent student = new MavenStudent();
@@ -44,5 +45,71 @@ public class MavenStudentTest {
         /*关闭session会话*/
         session.close();
     }
+    //99乘法表
+    @Test
+    public  void  TestStudent(){
+       for (int i=1;i<10;i++){
+            for (int j=1;j<=i;j++){
+                System.out.println(j+"*"+i+"="+(i*j)+'\n');
+            }
+
+       }
+    }
+    @Test
+    //10以内偶数之和
+    public  void  Tset2(){
+        int  sum=0;
+        for (int i=1;i<=10;i++){
+            if (i%2 == 1){
+                continue;
+            }
+            sum=sum+i;
+
+        }
+        System.out.println("==="+sum);
+
+    }
+    //直角三角形
+    @Test
+    public void Test3(){
+        for (int i=1;i<=3;i++){
+            for (int j=1;j<=i;j++){
+                System.out.println("*");
+            }
+            System.out.print("\t");
+        }
+
+    }
+    @Test
+    public void Test4(){
+        int num = 1000;
+        int count = 0;
+
+        if (num >= 0  && num<=999999999){
+            while(num != 0){
+                count++;
+                num/=10;
+            }
+            System.out.println("它是个"+ count+"位的数！");
+        } else{
+            System.out.println("输入有误！");
+        }
+
+    }
+    //循环练习
+    @Test
+    public void Test5(){
+        int score=new Scanner(System.in).nextInt();
+        int count = 0;
+        System.out.println("加分钱的成绩为"+score);
+        while (score<60){
+            score++;
+            count++;
+        }
+        System.out.println("加分后的成绩为"+score);
+        System.out.println("加分的次数为"+count);
+    }
+
 
 }
+
